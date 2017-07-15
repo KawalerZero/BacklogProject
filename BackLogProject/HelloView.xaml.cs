358714e027a0cffd59f2e7827288f5381748ccd6
KawalerZero <adamkowalczyk12@outlook.com>
2017-07-15 13:19:23 +0200

    AKO: Start project. Add solution and first approach.

---
+++ BackLogProject/HelloView.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BackLogProject
+{
+	/// <summary>
+	/// Interaction logic for HelloView.xaml
+	/// </summary>
+	public partial class HelloView : Window, INotifyPropertyChanged
+
+	{
+
+		private ICommand _buttonGoToMainCommand;
+		private ICommand _buttonExitCommand;
+		private ICommand _buttonOptionsCommand;
+
+
+		public HelloView()
+		{
+			InitializeComponent();
+			DataContext = this;
+			ButtonOk = new DelegateCommand(NextExecute);
+			ButtonOptions = new DelegateCommand(OptionsExecute);
+			ButtonExit = new DelegateCommand(ExitExecute);
+
+		}
+		public ICommand ButtonOk
+		{
+			get
+			{
+				return _buttonGoToMainCommand;
+			}
+			set
+			{
+				_buttonGoToMainCommand = value;
+				OnPropertyChanged();
+			}
+		}
+		public ICommand ButtonOptions
+		{
+			get
+			{
+				return _buttonOptionsCommand;
+			}
+			set
+			{
+				_buttonOptionsCommand = value;
+				OnPropertyChanged();
+			}
+		}
+		public ICommand ButtonExit
+		{
+			get
+			{
+				return _buttonExitCommand;
+			}
+			set
+			{
+				_buttonExitCommand = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private void NextExecute()
+		{
+			MainWindow main = new MainWindow();
+			main.Show();
+			ExitExecute();
+		}
+
+		private void OptionsExecute()
+		{
+			OptionsWindow options = new OptionsWindow();
+			options.Show();
+			ExitExecute();
+		}
+
+		private void ExitExecute()
+		{
+			this.Close();
+		}
+
+
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+	}
+}
---
+++ BackLogProject/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BackLogProject
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+
+	public partial class MainWindow : Window, INotifyPropertyChanged
+	{
+		private ICommand _addSomething;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			DataContext = this;
+			ButtonAdd = new DelegateCommand(AddSomethingNew);
+		}
+		public ICommand ButtonAdd
+		{
+			get
+			{
+				return _addSomething;
+			}
+			set
+			{
+				_addSomething = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public void AddSomethingNew()
+		{
+
+			lolo.Children.Add(new TextBox() { Text = "Babau" });
+		}
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
