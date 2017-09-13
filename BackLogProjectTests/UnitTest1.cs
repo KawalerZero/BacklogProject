be93b66ca8c0fc626a858a753a54cc298b401504
KawalerZero <adamkowalczyk12@outlook.com>
2017-09-13 21:29:38 +0200

    AKO: Add unit test project for check AppVeyor

---
+++ BackLogProjectTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BackLogProjectTests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.AreEqual(2, 5);
+		}
+		[TestMethod]
+		public void TestMethod2()
+		{
+			Assert.AreEqual(2, 2);
+		}
+	}
+}
